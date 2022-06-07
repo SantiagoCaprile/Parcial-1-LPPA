ebf3435759a037aa1809c546d04d55d98c2ff0eb
Santiago Caprile <santiagocaprile7@gmail.com>
2022-06-07 20:33:16 -0300

    El modal ya muestra toda la info del formulario

--- validations.js
+++ validations.js
@@ -6,8 +6,7 @@ window.onload = function () {
   const botonEnviar = document.getElementById("boton-enviar");
   const form = document.getElementById("formulario");
   const modal = document.getElementById("modal");
-  const btn = document.getElementById("myBtn");
-  const span = document.getElementsByClassName("close")[0];
+  const equis = document.getElementsByClassName("close")[0];
 
   // When the user clicks on the button, open the modal
   function mostrarModal(){
@@ -16,10 +15,47 @@ window.onload = function () {
     document.getElementById("apellido-final").innerText = "Apellido: " + apellido.value;
     document.getElementById("email-final").innerText = "Email: " + email.value;
     document.getElementById("edad-final").innerText = "Edad: " + edad.value + " años";
+    document.getElementById("sexo-final").innerText = "Sexo: " ;
+    if (document.getElementById("masculino").checked) {
+      document.getElementById("sexo-final").innerText += " Masculino";
+    } else if (document.getElementById("femenino").checked) {
+      document.getElementById("sexo-final").innerText += " Femenino";
+    } else if (document.getElementById("otro").checked) {
+      document.getElementById("sexo-final").innerText += " Otro";
+    }
+    document.getElementById("intereses-final").innerText = "Intereses: ";
+    if (document.getElementById("deportes").checked) {
+      document.getElementById("intereses-final").innerText += "- Deportes";
+    }
+    if (document.getElementById("musica").checked) {
+      document.getElementById("intereses-final").innerText += "- Música";
+    }
+    if (document.getElementById("juegos").checked) {
+      document.getElementById("intereses-final").innerText += "- Juegos";
+    }
+    if (document.getElementById("tecnologia").checked) {
+      document.getElementById("intereses-final").innerText += "- Tecnología";
+    }
+    document.getElementById("pais-final").innerText = "País: ";
+    switch (document.getElementById("pais").value) {
+      case "arg":
+        document.getElementById("pais-final").innerText += " Argentina";
+        break;
+      case "bra":
+        document.getElementById("pais-final").innerText += " Brasil";
+        break;
+      case "chi":
+        document.getElementById("pais-final").innerText += " Chile";
+        break;
+      case "uru":
+        document.getElementById("pais-final").innerText += " Uruguay";
+        break;
+    }
+    document.getElementById("comentario-final").innerText = "Comentario: \n" + document.getElementById("comentario").value;
   };
 
   // When the user clicks on <span> (x), close the modal
-  span.onclick = function () {
+  equis.onclick = function () {
     modal.style.display = "none";
   };
 
@@ -29,49 +65,6 @@ window.onload = function () {
       modal.style.display = "none";
     }
   };
-
-  function validarNombre() {
-    if (nombre.value.length < 3) {
-      nombre.classList.add("error-field");
-    } else {
-      nombre.classList.remove("error-field");
-    }
-  }
-  function validarApellido() {
-    if (apellido.value.length < 3) {
-      apellido.classList.add("error-field");
-    } else {
-      apellido.classList.remove("error-field");
-    }
-  }
-  function validarEmail() {
-    if (!email.value.match(/[a-z0-9]+@[a-z]+\.[a-z]/)) {
-      email.classList.add("error-field");
-    } else {
-      email.classList.remove("error-field");
-    }
-  }
-  function validarEdad() {
-    const valorEdad = Number(edad.value);
-    if (Number.isInteger(valorEdad)) {
-      if (edad.value < 0 || edad.value > 100) {
-        edad.classList.add("error-field");
-      } else {
-        edad.classList.remove("error-field");
-      }
-    } else {
-      edad.classList.add("error-field");
-    }
-  }
-  /* botonEnviar.addEventListener("click", function (e) {
-    e.preventDefault();
-    validarNombre();
-    validarApellido();
-    validarEmail();
-    validarEdad();
-  });
-  */
-
   ///////////////////////////////////////////////
   function esNombreValido() {
     if (nombre.value.length < 3) {
