2163ef11a8e9214a63f17e2f572a6f83d93821e6
Santiago Caprile <santiagocaprile7@gmail.com>
2022-06-08 22:10:38 -0300

    Comentarios finales en el readme y verificacion de consistencia en la estructura del codigo

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 Repositorio para primer parcial de LPPA.
 
 Alumno: Santiago Caprile
+
+Puede visualizar la página en:
+
+https://flyinsoup.github.io/Parcial-1-LPPA/
--- validations.js
+++ validations.js
@@ -13,7 +13,7 @@ window.onload = function () {
   const equis = document.getElementsByClassName("close")[0];
   const todosLosFieldsets = document.getElementsByTagName("fieldset");
 
-  function mostrarModal(){
+  function mostrarModal() {
     modal.style.display = "block";
     var frase = "Su nombre es " + nombre.value + " " + apellido.value + ".";
     var frase2 = "Vive en ";
@@ -52,10 +52,9 @@ window.onload = function () {
     if (document.getElementById("tecnologia").checked) {
       frase3 += "->Tecnología";
     }
-
     var frase4 = "A partir de ahora recibira todas las novedades en: ";
     frase4 += document.getElementById("email").value;
-    if(comentario.value != ""){
+    if (comentario.value != "") {
       var frase5 = "Ademas aclara que: " + comentario.value;
     }
     console.log(frase);
@@ -64,7 +63,7 @@ window.onload = function () {
     modalHeader.appendChild(document.createElement("p")).innerHTML = frase3;
     modalHeader.appendChild(document.createElement("p")).innerHTML = frase4;
     modalHeader.appendChild(document.createElement("p")).innerHTML = frase5;
-  };
+  }
   equis.onclick = function () {
     modal.style.display = "none";
   };
@@ -73,9 +72,8 @@ window.onload = function () {
       modal.style.display = "none";
     }
   };
-  ///////////////////////////////////////////////
-  function mostrarErrores(campo, error){
-    if(campo.parentElement.lastElementChild.classList.contains("error")){
+  function mostrarErrores(campo, error) {
+    if (campo.parentElement.lastElementChild.classList.contains("error")) {
       campo.parentElement.lastElementChild.innerText = error;
     } else {
       campo.parentElement.appendChild(document.createElement("p"));
@@ -83,13 +81,13 @@ window.onload = function () {
       campo.parentElement.lastElementChild.classList.add("error");
     }
   }
-  function limpiarErrores(fieldSet){
-    if(fieldSet.lastElementChild.classList.contains("error")){
+  function limpiarErrores(fieldSet) {
+    if (fieldSet.lastElementChild.classList.contains("error")) {
       fieldSet.lastElementChild.remove();
     }
   }
-  function limpiarTodosLosErrores(){
-    for(let i = 0; i < todosLosFieldsets.length; i++){
+  function limpiarTodosLosErrores() {
+    for (let i = 0; i < todosLosFieldsets.length; i++) {
       limpiarErrores(todosLosFieldsets[i]);
     }
   }
@@ -138,25 +136,25 @@ window.onload = function () {
     }
     return error;
   }
-  //("" && "") == ""
-  //las 4 funciones devuelven strings, por lo que si no hay errores, devuelve ""
-  //falta modificar el p de error para que si hay errores, muestre los de la función
-
   function erroresSexo() {
     let error = "";
-    if (!document.getElementById("masculino").checked &&
-    !document.getElementById("femenino").checked && 
-    !document.getElementById("otro").checked) {
+    if (
+      !document.getElementById("masculino").checked &&
+      !document.getElementById("femenino").checked &&
+      !document.getElementById("otro").checked
+    ) {
       error += "Debe seleccionar una opcion.\n";
     }
     return error;
   }
   function erroresIntereses() {
     let error = "";
-    if (!document.getElementById("deportes").checked &&
-    !document.getElementById("musica").checked && 
-    !document.getElementById("juegos").checked && 
-    !document.getElementById("tecnologia").checked) {
+    if (
+      !document.getElementById("deportes").checked &&
+      !document.getElementById("musica").checked &&
+      !document.getElementById("juegos").checked &&
+      !document.getElementById("tecnologia").checked
+    ) {
       error += "Debe seleccionar al menos una opcion.\n";
     }
     return error;
@@ -191,4 +189,4 @@ window.onload = function () {
       mostrarErrores(pais, erroresPais());
     }
   });
-}
+};
